ce52d649312cb2558ffda7e76baf0e3f2000bf9c
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2022-10-30 18:56:44 +0500

    Add files via upload

---
+++ blabla.js
@@ -0,0 +1,244 @@
+// 1
+
+// let num = 10;
+// if(num == 10){
+//     alert('Верно')
+// }else{
+//     alert('Не верно')
+// }
+
+// 2
+
+// let min = prompt("Введите число от 0 до 59")
+// if(min >= 0 && min <= 14){
+//     alert('первая')
+// };
+// if(min >= 15 && min <= 30){
+//     alert('вторая')
+// };
+// if(min >= 31 && min <= 45){
+//     alert('третью')
+// };
+// if(min >= 46 && min <= 59){
+//     alert('четвертая')
+// };
+
+// 3
+
+// let lang = "ru"
+// let obj = {
+//     ru:["ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"],
+//     en:["MN","TU","WE","TH","FR","SA","SU"],
+// }
+// let arr = obj[lang]
+// alert(arr)
+
+// 1     Работа с if-else
+
+// let a = prompt('Введите число')
+// if(a == 0){
+//     alert('Верно')
+// }
+
+// else{
+//     alert('Не верно')
+// }
+
+// 2
+
+// let a = prompt('Введите число')
+// if(a > 0){
+//     alert('Верно')
+// }
+
+// else{
+//     alert('Не верно')
+// }
+
+// 3
+
+// let a = prompt('Введите число')
+// if(a < 0){
+//     alert('Верно')
+// }
+
+// else{
+//     alert('Не верно')
+// }
+
+// 4
+
+// let a = prompt('Введите число')
+// if(a >= 0){
+//     alert('Верно')
+// }
+
+// else{
+//     alert('Не верно')
+// }
+
+// 5
+
+// let a = prompt('Введите число')
+// if(a <= 0){
+//     alert('Верно')
+// }
+
+// else{
+//     alert('Не верно')
+// }
+
+// 6
+
+// let a = prompt('Введите число')
+// if(a == 0){
+//     alert('Не верно')
+// }
+
+// else{
+//     alert('верно')
+// }
+
+// 7
+
+// let a = prompt('введите число')
+// let test = 3
+// if(a==test){
+//     alert('Верно')
+// }
+// else{
+//     alert('Не верно')
+// }
+
+// 8
+
+// let a = 1
+// if(a == 1){
+//     alert('Верно')
+// }
+// else{
+//     alert('Не верно')
+// }
+
+// 1 Работа с && (и) и || (или)
+// let a = prompt('Введите число')
+// if(a > 0 && a < 5){
+//     alert('Верно')
+// }
+// else{
+//     alert('Не верно')
+// }
+
+// 2
+
+// let a = prompt('Введите число')
+// if(a == 0 || a == 2){
+//     alert(Number(a) + 7)
+// }
+// else {alert(Number(a) / 10)}
+
+// 3
+
+// let a = prompt('Введите число')
+// let b = prompt('Введите число')
+// if(a <= 1 && b >= 3){
+//     alert(Number(a) + Number(b))
+// }
+// else alert(Number(a)-Number(b))
+
+// 4
+
+// let a = prompt('Введите число')
+// let b = prompt('Введите число')
+// if(a > 2 && a < 11){alert('Верно')}
+// if(b >= 6 && b < 14){alert('Верно')}
+// else{alert('не верно')}
+
+// 1 задачи на switch-case
+
+// let num = prompt('Введите число')
+// switch(num){
+//     case '1':
+//         alert('Зима')
+//         break;
+//     case '2':
+//         alert('Весна')
+//         break;
+//     case '3':
+//         alert('Лето')
+//             break;
+//     case '4':
+//         alert('Осень')
+//         break;
+//     default:
+//         alert('Число должно быть от 1 до 4')
+// }
+
+// 1 задачи
+// let day = prompt('Введите число от 1 до 31')
+// if(day < 11 && day > 0){
+//     alert('Первая')
+// }
+// if(day > 11 && day < 22){
+//     alert('Вторая')
+// }
+// if(day > 21 && day <= 31){
+//     alert('третью')
+// }
+// else{
+//     alert('Не верное число')
+// }
+
+// 2
+
+// let month = prompt('Введите число от 1 до 12')
+// if(month == 1 || month == 2 || month == 12){
+//     alert('Зима')
+// }
+// if(month == 3 || month == 4 || month == 5){
+//     alert('Весна')
+// }
+// if(month == 6 || month == 7 || month == 8){
+//     alert('Лето')
+// }
+// if(month == 9 || month == 10 || month == 11){
+//     alert('Осень')
+// }
+// else{
+//     alert('не верное число')
+// }
+
+// 3
+
+// let al = ['a','b','c','d','e']
+// if(al[0] == 'a'){
+//     alert('Да')
+// }
+// else{alert('Нет')}
+
+// 4
+
+// let num = ['1','2','3','4','5']
+// if(num[0] == 1 || num[0] == 2 || num[0] == 3){
+//     alert('Да')
+// }
+// else{
+//     alert('Нет')
+// }
+
+// 5
+
+// let  num = prompt('Введите число');
+// alert(Number(num[0])+Number(num[1])+Number(num[2]));
+
+// 6
+
+// let num = prompt('Введите шестизначное число')
+// let a = Number(num[0])+Number(num[1])+Number(num[2]);
+// let b = Number(num[3])+Number(num[4])+Number(num[5]);
+// if(a == b){
+//     alert('Да')
+// }
+// else{
+//     alert('Нет')
+// }
